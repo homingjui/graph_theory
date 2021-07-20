e6a87ed0ebb0ab311160ac2a18ab1eada87444c1
homingjui <richard.93600@gmail.com>
2021-07-21 02:29:18 +0800

    speed to 17 sec

--- pre_project.py
+++ pre_project.py
@@ -6,7 +6,7 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.inf)
-
+import time
 
 def perm(n,begin,end):
     global n_array,side_array
@@ -62,6 +62,8 @@ def sort(n):
                 flag = True
     return x
 
+
+
 ###############################################parm
 nodes = 8
 path = 'output.txt'
@@ -76,8 +78,8 @@ for i in range(1,nodes):
     circle.append([i,i+1])
 circle.append([1,nodes])
 circle = np.array([circle])
-
 permutation(nodes)
+n_array = np.array(n_array)
 # print len(n_array)
 # print side_array
 
@@ -105,10 +107,11 @@ print(np.shape(edge_list_array))
 # print(edge_list_X_array)
 npresult = np.array([])
 G_num = 0
+edge_num = len(edge_list_array[0])
 ################################################################ do all G
 for now_G in range(len(edge_list_array)):
     result = []
-    result_z_w = []
+    result_uni = []
     if now_G>0:
         if len(np.where(np.all(npresult == edge_list_array[now_G], axis=(1,2)))[0])>0:
             # print ("%d's G is equivalence"%now_G)
@@ -117,46 +120,27 @@ for now_G in range(len(edge_list_array)):
     G_num += 1
     ######################################find all combination
     x_array = np.array([X_cal(i) for i in edge_list_array[now_G]])
+    start = time.time()
 
     for now_iteration in range(iterations):
         new_edge_list = copy.deepcopy(edge_list_array[now_G])
-        # print new_edge_list
-        # print n_array[now_iteration]
-        #
-        # for i in range(len(new_edge_list)):
-        #     new_edge_list[i][0]=n_array[now_iteration][new_edge_list[i][0]-1]+1
-        #     new_edge_list[i][1]=n_array[now_iteration][new_edge_list[i][1]-1]+1
-        # print new_edge_list
 
-        for i in range(nodes):
-            new_edge_list[new_edge_list==i+1]=(n_array[now_iteration][i]+1)*10
-        new_edge_list/=10
-        new_edge_list_x = np.array([X_cal(i) for i in new_edge_list])
-        # print new_edge_list
-        # input()
-        z=1
-        w=1
-        # for i in range(len(new_edge_list-1)):
-        #     z *= np.prod(new_edge_list_x[i+1:]*(-1)+new_edge_list_x[i])/np.prod(x_array[i+1:]*(-1)+x_array[i])
-        w=np.prod(new_edge_list_x)/np.prod(x_array)
+        for i in range(edge_num):
+            new_edge_list[i][0]=n_array[now_iteration][new_edge_list[i][0]-1]+1
+            new_edge_list[i][1]=n_array[now_iteration][new_edge_list[i][1]-1]+1
 
-        # for i in range(len(new_edge_list)):
-        #     i_x_cal = X_cal(new_edge_list[i])
-        #     for j in range(i+1,len(new_edge_list)):
-        #         z *= (new_edge_list_x[i]-new_edge_list_x[j])/(x_array[i]-x_array[j])
-        #     w *=  X_cal(new_edge_list[i])/x_array[i]
-        result_z_w.append((z,(abs(z+1)+abs(w-1))))
-
-        result.append(sort(new_edge_list))
-
+        result_uni.append(sort(new_edge_list))
+        result.append(new_edge_list)
+    print time.time()-start
+    start = time.time()
     # result = np.array(result)
     # print np.where(np.all(result == result[0], axis=(1,2)))
 ##########################################filter
-    result,result_n =np.unique(result,axis=0,return_index=True)
-    # print("len result: %d"%(len(result)))
+    result_uni,result_n =np.unique(result_uni,axis=0,return_index=True)
+    print("len result: %d"%(len(result)))
 ##########################################save file
     record_file = open(path, 'a')
-    path_arr = 'output/AA'+str(G_num)+'.txt'
+    path_arr = 'output/Ab'+str(G_num)+'.txt'
     record_arr_file = open(path_arr, 'w')
     write_string="A"+str(G_num)+": "
     for i in edge_list_array[now_G]:
@@ -165,28 +149,33 @@ for now_G in range(len(edge_list_array)):
     record_file.write(write_string[:-2])
     record_file.write("\n")
     record_file.close()
+    write_string=""
+    for n_gaph in range(len(result_uni)):
+        write_string+="\n\n"+str(n_gaph)+", \tf="
 
-    for n_gaph in range(len(result)):
-        write_string="\n\n{}, \tf=".format(n_gaph)
-        for i in range(len(n_array[0])):
+        for i in range(nodes):
             write_string+=str(i+1)+":"+str(n_array[result_n[n_gaph]][i]+1)+",  "
         write_string += "\n"
-        for i in result[n_gaph]:
-            write_string += str(i)+", "
-        record_arr_file.write(write_string[:-2])
-        write_string = "\tZ="+str(result_z_w[result_n[n_gaph]][0])+", W="+str(result_z_w[result_n[n_gaph]][1])
-        record_arr_file.write(write_string)
+        write_string += np.array2string(result_uni[n_gaph]).replace("\n", "")
 
+        new_edge_list_x = np.array([X_cal(i) for i in result[result_n[n_gaph]]])
+
+        z=1
+        for i in range(edge_num):
+            z *= np.prod(new_edge_list_x[i+1:]*(-1)+new_edge_list_x[i])/np.prod(x_array[i+1:]*(-1)+x_array[i])
+
+        write_string += "\tZ="+str(z)+", W="+str(z)
+    record_arr_file.write(write_string)
     record_arr_file.close()
     ###########################################result
-    print("filted result num: %d"%(len(result)))
-
+    print("filted result num: %d"%(len(result_uni)))
     if now_G==0:
-        npresult = np.array(result)
+        npresult = np.array(result_uni)
     else:
-        npresult= np.vstack((npresult,result))
+        npresult= np.vstack((npresult,result_uni))
     print ("all f(G) num: %d"%np.shape(npresult)[0])
     # print np.shape(npresult)
+    print time.time()-start
 
 npresult=np.unique(npresult,axis=0)
 print ("all f(G) num: %d"%np.shape(npresult)[0])
